398dcd4f5fdd40f252e8bc6328449640cddb22b9
Bartosz Lozinski <lozinski.bart@gmail.com>
2026-05-26 17:26:35 +0200

    Renamed project

--- Flash.sh
+++ Flash.sh
@@ -2,4 +2,4 @@
 
 openocd -f /usr/share/openocd/scripts/interface/stlink.cfg \
         -f /usr/share/openocd/scripts/target/stm32l4x.cfg \
-        -c "program build/Core/DrivingRobot verify reset exit"
+        -c "program build/Core/EnvironmentStation verify reset exit"
