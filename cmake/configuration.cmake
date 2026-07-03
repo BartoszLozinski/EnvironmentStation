a35b0a080810bf70d1887da383df485b76ade3aa
Bartosz Lozinski <lozinski.bart@gmail.com>
2026-07-03 18:47:30 +0200

    added configure_embedded_project cmake function

--- cmake/configuration.cmake
+++ cmake/configuration.cmake
@@ -1,5 +1,36 @@
 function(configure_embedded_target target)
 
 
+    target_compile_options(${target}
+        PRIVATE
+            -Wall
+            -Werror
+            -Wextra
+            -Wpedantic
+            -fverbose-asm # additional comments for generated assembler code
+
+            
+            $<$<COMPILE_LANGUAGE:C>:-Wstrict-prototypes>
+
+            $<$<COMPILE_LANGUAGE:CXX>:-fno-rtti> # disable virtual class information used by dynamic_cast and typeid
+            $<$<COMPILE_LANGUAGE:CXX>:-fno-exceptions> # disable exceptions handling
+            
+            $<$<CONFIG:DEBUG>: -Og -g>
+            $<$<CONFIG:RELEASE>: -O3>
+    )
+
+    target_compile_definitions(${target}
+        PUBLIC 
+            STM32L476xx
+    )
+
+    set_target_properties(${target}
+        PROPERTIES
+            C_STANDARD 11
+            C_STANDARD_REQUIRED ON
+            CXX_STANDARD 20
+            CXX_STANDARD_REQUIRED ON
+    )
+
 
 endfunction()
